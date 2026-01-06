92adf692e374e8aa5430c5a01f6a5cbed5782045
Edwin Huamanttupa <edwinrhc@gmail.com>
2026-01-05 21:46:39 -0500

    feat Implementado la pagina en el controller hacer una prueba en la API

--- 02-Products-App/products-ms/src/products/products.service.ts
+++ 02-Products-App/products-ms/src/products/products.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Query } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaService } from '../prisma/prisma.service';
+
 
 @Injectable()
 export class ProductsService {
