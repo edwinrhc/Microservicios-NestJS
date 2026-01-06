e853f1fd851266540188dbe2cc8fc0bd93aaacd7
Edwin Huamanttupa <edwinrhc@gmail.com>
2026-01-05 23:08:53 -0500

    feat(Product): Método findOne - Retornamos el servicio de findOne

--- 02-Products-App/products-ms/src/products/products.controller.ts
+++ 02-Products-App/products-ms/src/products/products.controller.ts
@@ -29,8 +29,8 @@ export class ProductsController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.productsService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.findOne(id);
   }
 
   @Patch(':id')
