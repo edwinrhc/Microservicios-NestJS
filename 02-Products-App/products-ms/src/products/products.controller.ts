0ae630f4515f527792bc2d1a3cf7d446a83d547d
Edwin Huamanttupa <edwinrhc@gmail.com>
2026-01-05 23:08:32 -0500

    feat(Product): Método findAll - Retornamos el servicio de findAll agregando paginationDto

--- 02-Products-App/products-ms/src/products/products.controller.ts
+++ 02-Products-App/products-ms/src/products/products.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Delete,
   Query,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -24,8 +25,7 @@ export class ProductsController {
 
   @Get()
   findAll(@Query() paginationDto: PaginationDto) {
-    return paginationDto
-    // return this.productsService.findAll();
+    return this.productsService.findAll(paginationDto);
   }
 
   @Get(':id')
